826b2cd64a41e17dd590fa26e827ad6ec55827ec
Mindy Preston <meetup@yomimono.org>
2014-10-17 17:00:41 -0500

    whoops, missing a library in git

---
+++ lib/repeating_xor.mli
@@ -0,0 +1,4 @@
+val repeat_xor : string -> string -> string
+val repeat_xor_stream : string -> char Stream.t -> out_channel -> unit
+val rxor_exn : string -> string -> unit
+val repeat_xor_int_list : int list -> int list -> int list
