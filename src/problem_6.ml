50f4df296920fcbc5abee826a79b3fdcb244d70e
Mindy Preston <meetup@yomimono.org>
2014-10-17 11:13:13 -0500

    Sort out Hamming distances for different keysizes.

--- src/problem_6.ml
+++ src/problem_6.ml
@@ -1,25 +1,51 @@
 open Core.Std
 open Cmdliner
 
-let average_hamming_by_keysize ciphertext size =
-  if (String.length ciphertext) < (size * 2) then 
+let average_hamming_by_keysize ciphertext (size : int) =
+  if (List.length ciphertext) < (size * 2) then 
     `Invalid_argument "key size exceeds available text"
   else
-    let distance = (Hamming.distance (List.slice ciphertext 0 size)
-        (List.slice ciphertext size size)) / size 
-    in
-    distance
+    let (one, two) = List.split_n ciphertext size in
+    let distance = (Hamming.distance one (fst (List.split_n two size))) in
+    let normalized = (Int.to_float distance) /. (Int.to_float size) in
+    `Ok (normalized, size)
+
+let try_keysizes ciphertext =
+  let sizes = List.range ~stride:1 ~start:`inclusive ~stop:`inclusive 2 40 in
+  List.map ~f:(average_hamming_by_keysize ciphertext) sizes
 
 let break_xor file =
   In_channel.with_file file ~f:
     (fun f ->
        let str = (String.concat ~sep:"" (In_channel.input_lines f)) in
-       let decoded = Base64.int_list_of_base64 str in
-       match decoded with
-       | `Ok d -> Printf.printf "Decoded base64\n"
-                    Printf.printf "Checking Hamming distance 2"; 
-         Printf.printf "Normalized distance: %d\n" average_hamming_by_keysize 2
+       match Base64.int_list_of_base64 str with
        | `Invalid_argument s -> `Error (false, s)
+       | `Ok d -> Printf.printf "Decoded base64\n";
+         let distances = try_keysizes d in
+         (* comparator for sorting (hamming distance, keysize) tuples by 
+            distance, then keysize,
+            shuttling invalid_argument results to the end of the list *)
+         let comparator one two =
+           match (one, two) with 
+           (* Invalid_argument is larger than any `Ok value*)
+           | `Ok (p, a), `Invalid_argument _ -> -1
+           | `Invalid_argument _ , `Ok (p, a) -> 1
+           | `Invalid_argument _, `Invalid_argument _ -> 1
+           | `Ok (p, a), `Ok (q, b) -> 
+             match Float.compare p q with
+             | 0 -> Int.compare a b (* prefer smaller key sizes *)
+             | n -> n (* sort by hamming distance *)
+         in
+         let sd = List.sort distances ~cmp:comparator in
+         match (List.hd sd) with 
+         | Some `Ok best ->
+           Printf.printf "Best keysize: %d (%f)\n" (snd best) (fst best);
+           `Ok 0
+         | None -> 
+           `Error (false, "No best hamming distance candidate somehow")
+         | Some `Invalid_argument s -> 
+           `Error (false, "No keysize yielded meaningful results -
+           pathologically small ciphertext size?")
     )
 
 let file =
